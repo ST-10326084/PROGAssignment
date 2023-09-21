bd6152c8f0a483fc0993f6fb1ba38ea7807bd482
ST-10326084 <127187815+ST-10326084@users.noreply.github.com>
2023-09-21 13:07:05 +0200

    adding comments

--- JavaGame/src/Game/GameEntity.java
+++ JavaGame/src/Game/GameEntity.java
@@ -2,11 +2,13 @@ package Game;
 // All information sourced from Joyce Farrel,202, Java Programming Cengage 9th Edition
 // and Bro Code, 2020, Java Full Course for Free, Youtube (https://www.youtube.com/watch?v=xk4_1vDrzzo)
 
+// parents abstract class 
 public abstract class GameEntity {
     protected String[][] maze;
     protected int playerRow;
     protected int playerCol;
 
+    // constructor 
     public GameEntity(String[][] maze, int startRow, int startCol) {
         this.maze = maze;
         this.playerRow = startRow;
